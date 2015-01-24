aad4775509f1b71d82d0b42c69e3a6cfa2b20c49
kjgorman <kieran@kjgorman.com>
2015-01-24 13:10:32 +1300

    Chapter 2 and the beginning of chapter 3

---
+++ LeftistHeaps.hs
@@ -0,0 +1,61 @@
+module LeftistHeaps where
+
+data Heap a = Empty
+            | Heap { rank :: Int, element :: a, left :: Heap a, right :: Heap a }
+              deriving (Show)
+
+{- Ex 3.1
+  Given a leftist heap of size n, show that the length of the right
+  spine contains at most ⌊log(n+1)⌋ elements.
+
+  Assume we have a fully balanced tree, with depth x, then it's size
+  will be 2^x - 1. Every time that we add an element into the heap,
+  the leftist property ensures that we either balance the heap, or
+  we insert a new left child. Clearly for any unbalanced tree the
+  total size will be less than the 2^x - 1 limit, and when we add an
+  element to a balanced tree, we must create a new left child,
+  incrementing x which also ensures n < 2^x - 1.
+
+  So it must always be the case that:
+  n <= 2^x - 1
+  n + 1 <= 2^x
+  log(n + 1) <= x
+  and because x must be an integer we know we can floor the
+  logarithm to give:
+  ⌊log(n+1)⌋
+  So we know that we must have at most this many elements in a
+  fully balanced tree, which is the maximal situation for right
+  spine length, as the leftist property ensures any addition will
+  be a left child and increment x.
+-}
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge h Empty = h
+merge Empty h = h
+merge l r = if element l <= element r
+            then makeT (element l) (left l) $ merge (right l) r
+            else makeT (element r) (left r) $ merge l (right r)
+
+rank' :: Heap a -> Int
+rank' Empty = 0
+rank' h = rank h
+
+makeT :: a -> Heap a -> Heap a -> Heap a
+makeT a l r = if rank' l >= rank' r
+              then Heap (rank' r + 1) a l r
+              else Heap (rank' l + 1) a r l
+
+insert :: Ord a => a -> Heap a -> Heap a
+insert a h = merge (Heap 1 a Empty Empty) h
+
+findMin :: Heap a -> a
+findMin = element
+
+deleteMin :: Ord a => Heap a -> Heap a
+deleteMin h = merge (left h) (right h)
+
+-- Ex 3.2
+insert' :: Ord a => a -> Heap a -> Heap a
+insert' a Empty = Heap 1 a Empty Empty
+insert' a h | a <= element h = makeT a (left h) (insert' (element h) (right h))
+            | otherwise = makeT (element h) (left h) (insert' a (right h))
--- UnbalancedSet.hs
+++ UnbalancedSet.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module UnbalancedSet where
+
+import Control.Exception
+import Data.Typeable (Typeable)
 
 data Tree a = Empty | Node (Tree a) a (Tree a)
             deriving (Show)
@@ -28,3 +33,34 @@ member' x n@(Node _ e _) = go x n e
       go x Empty a = x == a
       go x (Node l e r) a | x < e = go x l a
                           | otherwise = go x r e
+
+data ElementAlreadyExists = ElementAlreadyExists
+                          deriving (Show, Typeable)
+
+instance Exception ElementAlreadyExists where
+
+-- Ex 2.3
+insert' :: Ord a => a -> Tree a -> IO (Tree a)
+insert' x Empty = return $ Node Empty x Empty
+insert' x n@(Node l e r) = do
+  res <- try (return $ go x n)
+  return $ case res of
+             Left (e :: ElementAlreadyExists) -> n
+             Right t -> t
+    where go x n | x < e = Node (insert x l) e r
+                 | x > e = Node l e (insert x r)
+                 | otherwise = throw ElementAlreadyExists
+
+-- Ex 2.5a
+complete :: Ord a => Int -> a -> Tree a
+complete 0 a = Node Empty a Empty
+complete s a = Node sub a sub
+    where sub = complete (s-1) a
+
+-- Ex 2.5b
+sized :: Ord a => Int -> a -> Tree a
+sized n a | n == 0 = Empty
+          | n == 1 = Node Empty a Empty
+          | even (n - 1) = let s = sized ((n - 1) `div` 2) a in Node s a s
+          | otherwise = let (l, r) = create2 ((n - 1) `div` 2) in Node l a r
+          where create2 n = (sized n a, sized (n+1) a)
